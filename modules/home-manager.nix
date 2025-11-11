d85fd072543de1de7910f361e76f9aee94e9f4fa
dylan <forde.dylan@gmail.com>
2025-11-11 00:00:17 +0000

    home manager sorted

--- modules/home-manager.nix
+++ modules/home-manager.nix
@@ -1,5 +1,9 @@
 # /home/dylan/nixos-config/modules/home-manager.nix
+{ inputs, ... }:
 {
+  # Pass the flake inputs to the home-manager modules
+  home-manager.extraSpecialArgs = { inherit inputs; };
+
   home-manager.users.dylan = {
     imports = [ ./home.nix ];
   };
